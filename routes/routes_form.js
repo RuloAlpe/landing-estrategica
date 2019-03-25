6882f5c77a6ac01ebbd63b5646967c27935e8206
Raul Alanis <rulosc07@gmail.com>
2019-03-25 16:05:40 -0600

    Mostrar datos del frontend

--- routes/routes_form.js
+++ routes/routes_form.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
   res.send("Inicio de servidor");
 });
 
+router.post('/create', (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    status: 'success',
+  });  
+});
+
 module.exports = router;
